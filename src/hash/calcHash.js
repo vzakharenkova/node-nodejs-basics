3bdf688ffa033c858bc09c8d33f7be4dcd3b4368
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-12-02 18:24:32 +0300

    feat: finish hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const callback = (error, data) => {
+    if (error) throw new Error('FS operation failed');
+
+    console.log(createHash('sha256').update(data).digest('hex'));
+  };
+
+  readFile(
+    path.join(__dirname, 'files/fileToCalculateHashFor.txt'),
+    'utf8',
+    callback
+  );
 };
 
 await calculateHash();
