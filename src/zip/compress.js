e0ea6d5772e6dfdafbbbe0d05ac5cb055fd7af92
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-12-02 18:38:58 +0300

    feat: finish zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const source = path.join(__dirname, 'files/fileToCompress.txt');
+const destination = path.join(__dirname, 'files/archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const readable = createReadStream(source);
+  const writable = createWriteStream(destination);
+
+  pipeline(readable, gzip, writable, (error) => {
+    if (error) {
+      console.error('Pipeline failed.', error);
+    }
+  });
 };
 
 await compress();
