cb796209c551433d0e078a20513cf8683c759b1b
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-12-02 18:21:02 +0300

    feat: finish cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envObj = process.env;
+  const result = [];
+
+  for (let key in envObj) {
+    if (key.startsWith('RSS_')) {
+      result.push(`${key}=${envObj[key]}`);
+    }
+  }
+
+  console.log(result.join('; '));
 };
 
 parseEnv();
